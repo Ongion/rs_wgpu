b67bf65f686ecf5e31cc9580b17b128a637fb8ba
Benjamin Paul <bepaul@microsoft.com>
2023-05-08 10:55:42 -0700

    Add Vertex struct and VERTICES

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,19 @@ struct State {
     colored_triangle_render_pipeline: wgpu::RenderPipeline,
 }
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
+    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
+    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+];
+
 impl State {
     // Creating some of the wgpu types requires async code
     async fn new(window: Window) -> Self {
