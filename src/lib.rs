8717a1d0ac901a237edf3df4f09533c1e6a73e9d
Benjamin Paul <bepaul@microsoft.com>
2023-05-08 10:55:42 -0700

    Make run async

--- src/lib.rs
+++ src/lib.rs
@@ -112,7 +112,7 @@ impl State {
 }
 
 #[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
-pub fn run() {
+pub aync fn run() {
     cfg_if::cfg_if! {
         if #[cfg(target_arch = "wasm32")] {
             std::panic::set_hook(Box::new(console_error_panic_hook::hook));
@@ -145,6 +145,8 @@ pub fn run() {
             })
             .expect("Couldn't append canvas to document body.");
     }
+
+    let mut state = State::new(window).await;
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
